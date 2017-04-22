4cf23769b34bb8d9742498ddeaf348b793c79678
mayur-hamirani <hamimayur1@gmail.com>
2017-04-21 23:20:47 -0700

    Bluetooth & Wifi Chat
    Simple messaging App lets you to chat using bluetooth and wi-fi

---
+++ app/build/generated/source/buildConfig/androidTest/debug/info/devexchanges/bluetoothchatapp/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.devexchanges.bluetoothchatapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "info.devexchanges.bluetoothchatapp.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
